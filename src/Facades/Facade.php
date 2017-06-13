68df3390cb689317923fd8351a6b44daaf593f7f
Gilberto Junior <gjunior@tray.net.br>
2017-06-13 15:37:28 +0000

    Correções componente

--- src/Facades/Facade.php
+++ src/Facades/Facade.php
@@ -3,12 +3,17 @@
 namespace TrayLabs\InfluxDB\Facades;
 
 use Illuminate\Support\Facades\Facade as LaravelFacade;
-use InfluxDB\Client as InfluxClient;
+use InfluxDB\Database as InfluxDB;
 
 class Facade extends LaravelFacade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
-        return InfluxClient::class;
+        return InfluxDB::class;
     }
 }
