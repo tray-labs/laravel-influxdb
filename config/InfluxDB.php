805de6238bae7243ad9364ee775b39b24cdb0136
dsandrade <danielsanto.andrade@hotmail.com>
2017-06-13 11:37:43 -0300

    Correção formatos config

--- config/InfluxDB.php
+++ config/InfluxDB.php
@@ -2,11 +2,11 @@
 
 return [
     'host' => env('INFLUXDB_HOST', 'localhost'),
-    'port' => env('INFLUXDB_PORT', '8086'),
+    'port' => env('INFLUXDB_PORT', 8086),
     'username' => env('INFLUXDB_USER', ''),
     'password' => env('INFLUXDB_PASSWORD', ''),
-    'ssl' => env('INFLUXDB_SSL', 'false'),
-    'verifySSL' => env('INFLUXDB_VERIFYSSL', 'false'),
+    'ssl' => env('INFLUXDB_SSL', false),
+    'verifySSL' => env('INFLUXDB_VERIFYSSL', false),
     'timeout' => env('INFLUXDB_TIMEOUT', '0'),
     'dbname' => env('INFLUXDB_DBNAME', ''),
 ];
